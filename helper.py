9ef3a29f3532ff954ac58d7b5de930799e3a7c4c
Raj Tiwari <98082499+rajtiwariee@users.noreply.github.com>
2023-06-05 21:16:24 +0530

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+#plot loss curves to show training and accuraccy loss seperately
+#let's create a function to plot loss curves
+def plot_loss_curves(history):
+  """
+  pass the history and it returns the loss curves and validation accuracy
+
+  """
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
+
+  accuracy = history.history['accuracy']
+  val_accuracy = history.history['val_accuracy']
+
+  epochs = range(len(history.history['loss']))
+  
+  # Plot loss
+  plt.plot(epochs, loss, label='training_loss')
+  plt.plot(epochs, val_loss, label='val_loss')
+  plt.title('Loss')
+  plt.xlabel('Epochs')
+  plt.legend()
+
+  # Plot accuracy
+  plt.figure()
+  plt.plot(epochs, accuracy, label='training_accuracy')
+  plt.plot(epochs, val_accuracy, label='val_accuracy')
+  plt.title('Accuracy')
+  plt.xlabel('Epochs')
+  plt.legend();
